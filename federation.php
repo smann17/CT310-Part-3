2cad4f5b95b16ea92b48d44aee917cb185b6056d
darkbobo <curtis.mayott@gmail.com>
2015-05-04 20:50:54 -0600

    worked on federation.php updated purpose.php to resemble json

--- federation.php
+++ federation.php
@@ -7,11 +7,51 @@
 	require_once("user.php");
 	$util = new util();
 ?>
+
 <div class="white"></div>
 <div class="leftContent" style= "color :white;">
 	<p style="font-size:20px;">Federation of Social Networking Sites</p><br>
+	<div id="federation_display"></div>
+	
 	
 </div>
+
+<script type="text/javascript">
+	var request = new XMLHttpRequest();
+	var url = "./purpose.php";
+	
+	request.onreadystatechange = function(){
+		if(request.readyState == 4 && request.status == 200){
+			var results = JSON.parse(request.responseText);
+			alert(request.responseText);
+			parseResults(results);
+		}else if(request.readyState == 4 && request.status != 200){
+			document.getElementById("federation_display").innerHTML = "<p>Error retrieving partner sites</p>";
+		}
+	}
+	request.timeout = 5000;
+	request.ontimeout = function() {
+		document.getElementById("federation_display").innerHTML = "<p>Error :: Time Out</p>";
+	}
+	request.open("GET", url, true);
+	request.send();
+	
+	function parseResults(arr){
+		//
+		var out = "";
+		var i;
+		alert(arr.length);
+		for(i = 0; i < arr.length; i++){
+			// handle if a single partner site is down
+			out += '<a href="' + arr[i].purpose + '">' + arr[i].purpose + '</a>';
+			if(i % 3 == 0){
+				out += '<br>';
+			}
+		}
+		document.getElementById("federation_display").innerHTML = out;
+	}
+</script>
+
 <?php
 	include_once("inc/rightContent.php");
 	include("inc/footer.php");
--- purpose.php
+++ purpose.php
@@ -1,6 +1,6 @@
 <?php
 	header('Content-Type: text/json');
-	$sitepurpose=array("purpose"=>"This purpose of the site is to give you a social 
-		media site where you can meet friends");
+	$sitepurpose=array('{"purpose":"This purpose of the site is to give you a social 
+		media site where you can meet friends"}');
 	echo json_encode($sitepurpose);
 ?>
